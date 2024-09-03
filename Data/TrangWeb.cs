2e0383ba38850ab4c0aab36c6ffa9fa7f9dfe8ff
Truong Nguyen <vonguyen.0407@gmail.com>
2024-09-03 20:20:33 +0700

    # 01 - Khởi tạo Project Ecomerce - 👉Ecommerce

---
+++ Data/TrangWeb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ECommerceMVC.Data
+{
+    public partial class TrangWeb
+    {
+        public TrangWeb()
+        {
+            PhanQuyen = new HashSet<PhanQuyen>();
+        }
+
+        public int MaTrang { get; set; }
+        public string TenTrang { get; set; }
+        public string Url { get; set; }
+
+        public virtual ICollection<PhanQuyen> PhanQuyen { get; set; }
+    }
+}
