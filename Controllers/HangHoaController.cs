6055fcc0045e9afc690e656716373f0e55a707e5
Truong Nguyen <vonguyen.0407@gmail.com>
2024-09-04 21:24:04 +0700

    bai 4 - xong phan xem danh sach san pham

--- Controllers/HangHoaController.cs
+++ Controllers/HangHoaController.cs
@@ -2,15 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ECommerceMVC.Data;
 using Microsoft.AspNetCore.Mvc;
+
+using ECommerceMVC.ViewModels;
 
 namespace ECommerceMVC.Controllers
 {
     public class HangHoaController : Controller
     {
-        public IActionResult Index(int? loai)
+        private readonly hshop2023Context db;
+        public HangHoaController(hshop2023Context context) => db = context;
+        public IActionResult Index(int? category)
         {
-            return View();
+            //lay tat ca danh sach hang hoa
+            var hangHoas = db.HangHoa.AsQueryable();
+
+            //neu co truyen them loai
+            if (category.HasValue)
+            {
+                //lay danh sach dua tren ma loai
+                hangHoas = db.HangHoa.Where(p => p.MaLoai == category.Value);
+            }
+
+            var result = hangHoas.Select(p => new HangHoaViewModel
+            {
+                MaHH = p.MaHh,
+                TenHH = p.TenHh,
+                Gia = p.DonGia ?? 0, // co the bang 0
+                Hinh = p.Hinh ?? "",
+                MoTaNgan = p.MoTaDonVi ?? "",
+                TenLoai = p.MaLoaiNavigation.TenLoai // nhay qua bang loai lay ten loai                
+            });
+
+            return View(result);
         }
     }
 }
---
+++ ViewModels/HangHoaViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceMVC.ViewModels
+{
+    public class HangHoaViewModel
+    {
+        public int MaHH { get; set; }
+        public string TenHH { get; set; }
+        public string Hinh { get; set; }
+        public double Gia { get; set; }
+        public string MoTaNgan { get; set; }
+        public string TenLoai { get; set; }
+    }
+}
