6194cbf6686c5ef63621231587a2bf6404d4990f
Truong Nguyen <vonguyen.0407@gmail.com>
2024-09-04 20:19:37 +0700

    # 03 - Tạo Menu hàng hóa

---
+++ ViewComponents/CategoryViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using ECommerceMVC.Data;
+using ECommerceMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceMVC.ViewComponents
+{
+    public class CategoryViewComponent : ViewComponent
+    {
+        // ket noi co so du lieu
+        private readonly hshop2023Context db;
+        public CategoryViewComponent(hshop2023Context context) => db = context;
+
+        public IViewComponentResult Invoke()
+        {
+            var data = db.Loai.Select(lo => new CategoryViewModel
+            {
+                MaLoai = lo.MaLoai, TenLoai = lo.TenLoai, SoLuong = lo.HangHoa.Count()
+            }).OrderBy(p => p.SoLuong);
+            return View(data); //Default.cshtml
+            //return View("Default", data); 
+        }
+    }
+}
